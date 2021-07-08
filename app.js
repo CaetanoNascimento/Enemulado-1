3cd75a6f177d2cd947eb2fcdecf8e04b405cf100
Pedro Paulo <pedropaulolins9@gmail.com>
2021-07-07 21:34:33 -0300

    caderno principal 07072020

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({ extended: false })); // dados simples
+app.use(bodyParser.json()); // json de entrada do body
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.use(express.static(__dirname+'/'));
+
+module.exports = app;
