c2c38ac49be4bc9b0c2bd5a9b1eefca58897d76d
Helen Nascimento <helen.hmns88@gmail.com>
2021-07-28 16:54:05 -0300

    quarta 280721 html, css , js

--- public/js/script.js
+++ public/js/script.js
@@ -32,8 +32,105 @@ function mousesai3(){
 }
 
 function mudatexto(elemento){
-   console.log(elemento.children[2].innerText)
- document.getElementById("myP").innerHTML = "novo"
 
+   console.log(elemento.children[2].innerText)
+
+   let nome = elemento.children[2].innerText
+
+   if(nome ==  "Caetano Nascimento"){
+
+    document.getElementById("myP").innerHTML = "Usar o ENEMULADO me salvou porque não tinha outra ferramenta que encaixava tão bem com o meu perfil. Espero que muitas pessoas possam também encontrar esse site"
+    document.getElementById("titulo").innerHTML = "ME SALVOU!"
+   }
+
+   if(nome ==  "Pepas El Mago"){
+
+    document.getElementById("myP").innerHTML = "Texto de Pepas"
+    document.getElementById("titulo").innerHTML = "AAAAEEE"
+    
+
+   }
+
+   if(nome ==  "Juan Euzinho"){
+
+    document.getElementById("myP").innerHTML = "Texto de Euzis"
+    document.getElementById("titulo").innerHTML = "VALEUUUUUUU"
+
+   }
+
+}
+
+function criar() {
+    /*console.log(document.getElementById("nome").value);
+    console.log(document.getElementById("email").value);
+    console.log(document.getElementById("cpf").value);
+    console.log(document.getElementById("telefone").value);
+    console.log(document.getElementById("senha").value);
+    console.log(document.getElementById("confirmar").value);*/
+
+    let senha = document.getElementById("senha").value
+    let confirmar = document.getElementById("confirmar").value
+
+    let login = {}
+
+    login.nome = document.getElementById("nome").value;
+    login.email = document.getElementById("email").value;
+    login.cpf = document.getElementById("cpf").value;
+    login.telefone = document.getElementById("telefone").value;
+    login.senha = document.getElementById("senha").value;
+    login.confirmar = document.getElementById("confirmar").value
+
+
+    if (senha == confirmar) {
+
+        adicionar(login)
+
+
+
+    } else {
+        alert('senha incorreta');
+    }
+
+}
+
+
+async function adicionar(login) {
+    try {
+
+        /*const formData = new FormData();
+        // const fileField = document.querySelector('input[type="file"]');
+
+        formData.append('nome', login.nome);
+        formData.append('email', login.email);
+        // formData.append('imagem_produto', fileField.files[0]);
+        formData.append('cpf', login.cpf);
+        formData.append('telefone', login.telefone);
+        formData.append('senha', login.senha);
+        formData.append('confirmar_senha', login.confirmar);
+
+        for (let [key, value] of formData.entries()) { 
+            console.log(key, value);
+          }
+*/
+
+
+        fetch('http://localhost:3000/cadastro', {
+            method: 'POST',
+            body: JSON.stringify(JSON.parse(login)),
+             headers: {
+                 "Content-Type": "application/json; charset=utf-8"
+             }
+
+        }).then(result => {
+            return result.json();
+        }).then(data => {
+           // this.listaTabela();
+           console.log(data);
+            // this.arrayProdutos.push(produto);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
