bc58177a2ba829fa90b449ce9aeb2c1c0162be4b
Helen Nascimento <helen.hmns88@gmail.com>
2021-09-30 19:49:05 -0300

    rai longee

--- public/js/script.js
+++ public/js/script.js
@@ -120,7 +120,7 @@ function logout() {
         localStorage.setItem("ourToken", null);
 
         location.assign('/login');
-        
+
     });
 }
 
@@ -187,7 +187,7 @@ function onSignIn(googleUser) {
         }
     };
     xhr.send(JSON.stringify({ token: id_token }));
-    
+
 }
 
 
@@ -204,7 +204,7 @@ function testetoken() {
 }
 
 function limpartoken() {
-   
+
 
     localStorage.setItem("Nome do usuario", null)
     localStorage.setItem("id do usuario", null)
@@ -400,21 +400,26 @@ let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 
-// closeBtn.addEventListener("click", ()=>{
-//   sidebar.classList.toggle("open");
-//   menuBtnChange();//calling the function(optional)
-// });
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+});
 
-// searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-//   sidebar.classList.toggle("open");
-//   menuBtnChange(); //calling the function(optional)
-// });
+searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+});
 
-// following are the code to change sidebar button(optional)
+//following are the code to change sidebar button(optional)
 function menuBtnChange() {
     if (sidebar.classList.contains("open")) {
         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
     } else {
         closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
     }
+}
+
+
+function correto(){
+    document.getElementById("ok").src = "../img/Vector.png"
 }
